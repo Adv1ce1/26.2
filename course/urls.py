ccfe9e9d45fd3909460ed133b1812a4c717ac41e
Adv1ce1 <135262373+Adv1ce1@users.noreply.github.com>
2023-10-30 15:16:35 +0300

    add subscribe

---
+++ course/urls.py
@@ -0,0 +1,21 @@
+from rest_framework import routers
+from django.urls import path
+from course.views.course import CourseViewSet
+from course.views.lesson import LessonListAPIView, LessonCreateAPIView, LessonDestroyAPIView, LessonUpdateAPIView, \
+    LessonRetrieveAPIView
+from course.views.subscription import SubscribeCourseView, UnsubscribeCourseView
+
+app_name = 'course'
+
+router = routers.SimpleRouter()
+router.register('course', CourseViewSet)
+urlpatterns = [
+                  path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+                  path('lesson/create', LessonCreateAPIView.as_view(), name='lesson-create'),
+                  path('lesson/delete/<int:pk>', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+                  path('lesson/update/<int:pk>', LessonUpdateAPIView.as_view(), name='lesson-update'),
+                  path('lesson/<int:pk>', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
+                  path('subscribe/<int:course_id>/', SubscribeCourseView.as_view(), name='subscribe-course'),
+                  path('unsubscribe/<int:course_id>/', UnsubscribeCourseView.as_view(), name='unsubscribe-course'),
+
+              ] + router.urls
